ec67242f6903b84b70bf3d6aa1f44fa4027a5aeb
Khaled Amr <khaledamr520@gmail.com>
2025-05-05 16:13:50 +0300

    dist ignore

--- dist/modules/profiles/profile.route.js
+++ dist/modules/profiles/profile.route.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const profile_controller_1 = require("./profile.controller");
+const authenticateToken_1 = require("../../middlewares/authenticateToken");
 const router = (0, express_1.Router)();
-router.get("/:uid", profile_controller_1.getUserProfile);
+router.get("/:uid", authenticateToken_1.authenticateToken, profile_controller_1.getUserProfile);
 exports.default = router;
