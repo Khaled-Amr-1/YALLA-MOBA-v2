1e7eb731ae5124a5857e35c6f11f28bdc165521c
Khaled Amr <khaledamr520@gmail.com>
2025-05-21 15:51:05 +0300

    a

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -8,6 +8,11 @@ const user_route_1 = __importDefault(require("../modules/users/user.route"));
 const profile_route_1 = __importDefault(require("../modules/profiles/profile.route"));
 const post_route_1 = __importDefault(require("../modules/posts/post.route"));
 const router = (0, express_1.Router)();
+router.use('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to the API',
+    });
+});
 router.use('/users', user_route_1.default);
 router.use('/profiles', profile_route_1.default);
 router.use('/posts', post_route_1.default);
