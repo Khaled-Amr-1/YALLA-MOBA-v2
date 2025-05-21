fb9a1fc090a4d5ad314293a512ee9ae644767b3e
Khaled Amr <khaledamr520@gmail.com>
2025-05-21 16:14:50 +0300

    asdfadfs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,16 +2,15 @@ import { Router } from 'express';
 import userRoutes from '../modules/users/user.route';
 import profileRoutes from '../modules/profiles/profile.route';
 import postRoutes from "../modules/posts/post.route";
-
 const router = Router();
 
-router.use('/', (req, res) => {
+
+router.use('/users', userRoutes);
+router.use('/profiles', profileRoutes);
+router.use('/posts',postRoutes);
+router.get('/', (req, res) => {
   res.status(200).json({
     message: 'Welcome to the API',
   });
 });
-router.use('/users', userRoutes);
-router.use('/profiles', profileRoutes);
-router.use('/posts',postRoutes);
-
 export default router;
