dbec95cef0a4b5a25d7290f32f9690f783986885
Khaled Amr <khaledamr520@gmail.com>
2025-05-21 16:53:08 +0300

    asdfasdfasdfasf

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,13 @@
-import app from '../src/app';
-export default app;
+// api/index.ts
+import express from 'express';
+import serverless from 'serverless-http';
+
+const app = express();
+app.use(express.json());
+
+// Register route
+app.post('/users/register', (req, res) => {
+  res.status(200).json({ message: "User registered!" });
+});
+
+export default serverless(app);
--- dist/api/index.js
+++ dist/api/index.js
@@ -3,5 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("../src/app"));
-exports.default = app_1.default;
+// api/index.ts
+const express_1 = __importDefault(require("express"));
+const serverless_http_1 = __importDefault(require("serverless-http"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+// Register route
+app.post('/users/register', (req, res) => {
+    res.status(200).json({ message: "User registered!" });
+});
+exports.default = (0, serverless_http_1.default)(app);
