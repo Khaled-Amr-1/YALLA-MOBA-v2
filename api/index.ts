b8d95bd01b55cdab2fd01a69615494ad318efa95
Khaled Amr <khaledamr520@gmail.com>
2025-05-22 21:10:35 +0300

    2 serverless

--- api/index.ts
+++ api/index.ts
@@ -2,12 +2,11 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import app from '../src/app';
 import { createServer, IncomingMessage, ServerResponse } from 'http';
 
-const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  app(req, res);
-});
-
-server.listen();
-
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+    app(req, res);
+  });
+
+  server.listen();
   server.emit('request', req, res);
 }
